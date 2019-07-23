9597c7f3cd4a4a804344cd53185f637755d44810
cai-yang <yc631207248@live.com>
2019-07-23 23:24:02 +0800

    初始化引用

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>|
-    <router-link to="/myrica">Myrica</router-link>
-    <router-view />
+    <el-container>
+      <el-header style="background-color:#c0c0c0;">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>|
+        <router-link to="/myrica">Myrica</router-link>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <side-menu />
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import SideMenu from "./components/MenuAside.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    SideMenu
   }
 };
 </script>
@@ -25,6 +38,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
